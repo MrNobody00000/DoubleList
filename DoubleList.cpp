13b371bb99a120f75b8f6b980914ce6934b2e1da
MrNobody00000 <mylnikov-vadim@mail.ru>
2021-11-10 15:14:07 +0300

    f

--- DoubleList.cpp
+++ DoubleList.cpp
@@ -8,20 +8,20 @@ public:
     Node *prev;
 };
 
-template<class Ttype, int size> 
+template<class Ttype> 
 class Dlist{
     Node *Head;
     Node *Tail;
 public:
-    DList():Head(NULL),Tail(NULL);
+    DList():Head(NULL),Tail(NULL){};
     ~Dlist();
     void show();
     void add(Ttype data);
 
 };
 
-template<class Ttype,int size> 
-Dlist<Ttype,size>::~Dlist(){
+template<class Ttype> 
+Dlist<Ttype>::~Dlist(){
     while(Head){
         Tail = Head->next;
         delete Head;
@@ -29,12 +29,49 @@ Dlist<Ttype,size>::~Dlist(){
     }
 }
 
-template<class Ttype,int size>
-void Dlist<Ttype,size>::add(Ttype data){
+template<class Ttype>
+void Dlist<Ttype>::add(Ttype data){
     Node *temp = new Node;
     temp->next = NULL;
     temp->x = data;
     if(Head != NULL){   // mb if(Head)
-        
+        temp->prev = NULL;
+        Tail->next = temp;
+        Tail = temp;
     }
+    else {
+        temp->prev = NULL;
+        Head = Tail = temp;
+    }
+}
+
+template<class Ttype>
+void Dlist<Ttype>::show(){
+    Node *temp=Tail; 
+    while (temp != NULL){
+        cout << temp->x << " ";
+        temp = temp->Prev;  
+    }
+    cout << "\n";
+
+    temp = Head;
+    while (temp != NULL) {
+        cout << temp->x << " ";
+        temp = temp->Next;
+    }
+    cout << "\n";
+}
+
+int main ()
+{
+
+ Dlist<int> lst; //Объявляем переменную, тип которой есть список
+ lst.add(100); //Добавляем в список элементы
+ lst.add(200);
+ lst.add(900);
+ lst.add(888);
+ 
+ lst.show(); //Отображаем список на экране
+
+    return 0;
 }
